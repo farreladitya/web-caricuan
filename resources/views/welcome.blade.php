875ac981161c0abbae5883fe21b1aa4d8b5e6ed5
raihandanaufal <raihandanaufal@gmail.com>
2022-02-28 21:52:07 +0700

    update plan my future

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,8 +173,8 @@ https://templatemo.com/tm-570-chain-app-dev
               <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
                 <div class="row">
                   <div class="col-lg-12">
-                    <h2>Get The Latest App From App Stores</h2>
-                    <p>Chain App Dev is an app landing page HTML5 template based on Bootstrap v5.1.3 CSS layout provided by TemplateMo, a great website to download free CSS templates.</p>
+                    <h2 style="font-size: 100px">Cari.Cuan</h2>
+                    <p>Cari.Cuan adalah platform untuk menghubungkan para pelamar kerja dan fresh <br> graduate dengan para penyedia lapangan pekerjaan dengan rekomendasi pelatihan <br> yang dapat disesuaikan dengan Plan My Future Test </p>
                   </div>
                   <div class="col-lg-12">
                     <div class="white-button first-button scroll-to-section">
@@ -203,9 +203,9 @@ https://templatemo.com/tm-570-chain-app-dev
       <div class="row">
         <div class="col-lg-8 offset-lg-2">
           <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
-            <h4>Amazing <em>Services &amp; Features</em> for you</h4>
+            <h4>Plan <em>My Future</em> Test</h4>
             <img src="assets/images/heading-line-dec.png" alt="">
-            <p>If you need the greatest collection of HTML templates for your business, please visit <a rel="nofollow" href="https://www.toocss.com/" target="_blank">TooCSS</a> Blog. If you need to have a contact form PHP script, go to <a href="https://templatemo.com/contact" target="_parent">our contact page</a> for more information.</p>
+            <p>Luasnya bidang lapangan pekerjaan akibat perkembangan <br> teknologi menjadi salah satu kendala dalam penentuan pilihan. <br> Kami hadir membantu anda menentukan pilihan melalui Plan My Future Test</p>
           </div>
         </div>
       </div>
@@ -215,31 +215,22 @@ https://templatemo.com/tm-570-chain-app-dev
         <div class="col-lg-3">
           <div class="service-item first-service">
             <div class="icon"></div>
-            <h4>App Maintenance</h4>
+            <h4>Ketahui Keahlian Anda</h4>
             <p>You are not allowed to redistribute this template ZIP file on any other website.</p>
-            <div class="text-button">
-              <a href="#">Read More <i class="fa fa-arrow-right"></i></a>
-            </div>
           </div>
         </div>
         <div class="col-lg-3">
           <div class="service-item second-service">
             <div class="icon"></div>
-            <h4>Rocket Speed of App</h4>
+            <h4>Soal Psikologi Terbaik</h4>
             <p>You are allowed to use the Chain App Dev HTML template. Feel free to modify or edit this layout.</p>
-            <div class="text-button">
-              <a href="#">Read More <i class="fa fa-arrow-right"></i></a>
-            </div>
           </div>
         </div>
         <div class="col-lg-3">
           <div class="service-item third-service">
             <div class="icon"></div>
-            <h4>Multi Workflow Idea</h4>
+            <h4>30 Menit Saja</h4>
             <p>If this template is beneficial for your work, please support us <a rel="nofollow" href="https://paypal.me/templatemo" target="_blank">a little via PayPal</a>. Thank you.</p>
-            <div class="text-button">
-              <a href="#">Read More <i class="fa fa-arrow-right"></i></a>
-            </div>
           </div>
         </div>
         <div class="col-lg-3">
@@ -247,9 +238,6 @@ https://templatemo.com/tm-570-chain-app-dev
             <div class="icon"></div>
             <h4>24/7 Help &amp; Support</h4>
             <p>Lorem ipsum dolor consectetur adipiscing elit sedder williamsburg photo booth quinoa and fashion axe.</p>
-            <div class="text-button">
-              <a href="#">Read More <i class="fa fa-arrow-right"></i></a>
-            </div>
           </div>
         </div>
       </div>
