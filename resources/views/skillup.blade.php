dfe9a9e57fb816868574cae0c20192ff69f77c0d
NurAida008 <aida.nurfebrianti@gmail.com>
2022-05-16 11:40:17 +0700

    update16-05

--- resources/views/skillup.blade.php
+++ resources/views/skillup.blade.php
@@ -34,4 +34,68 @@
       </div>
     </div>
   </div>
+
+  <div id="about" class="about-us section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading">
+            <h4>Skill Up <em>Course</em> </h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>Reach a bright future by honing skills according to your talents and interests</p>
+          </div>
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">With the Best Mentor</a></h4>
+                <p>Certified Mentors</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Video Access</a></h4>
+                <p>Everywhere & Anytime</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Create Schedule</a></h4>
+                <p>Customize your Schedule</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Latest Video</a></h4>
+                <p>Everyday</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <div class="service-item first-service">
+          <p>Plan My Future helps you to identify your interests, talents and skills that will be useful in the world of work</p>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="service-item second-service">
+          <p>Plan My Future provides psychological questions that have been tested in various psychological tests in Indonesia</p>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="service-item third-service">
+          <p>Plan My Future provides psychological test results in less than a week</p>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
