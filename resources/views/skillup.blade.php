9fc989674294c8758fec923b94f921473a30a8f8
NurAida008 <aida.nurfebrianti@gmail.com>
2022-05-17 22:27:17 +0700

    nambahlagi17/05

--- resources/views/skillup.blade.php
+++ resources/views/skillup.blade.php
@@ -15,20 +15,20 @@
       </div>
     </div>
     <div class="container">
-      <div class="row">
-        <div class="col-lg-3">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-3 mx-4">
           <div class="service-item first-service">
             <img src="assets/images/xl.png" style="width: 28%; height: 28%" alt="">
             <p>Plan My Future helps you to identify your interests, talents and skills that will be useful in the world of work</p>
           </div>
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-3 mx-4">
           <div class="service-item second-service">
             <img src="assets/images/xl.png" style="width: 28%; height: 28%" alt="">
             <p>Plan My Future provides psychological questions that have been tested in various psychological tests in Indonesia</p>
           </div>
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-3 mx-4">
           <div class="service-item third-service">
             <img src="assets/images/xl.png" style="width: 28%; height: 28%" alt="">
             <p>Plan My Future provides psychological test results in less than a week</p>
@@ -43,10 +43,16 @@
       <div class="row">
         <div class="col-lg-6 align-self-center">
           <div class="section-heading">
-            <img src="assets/images/xl.png" style="width: 15%; height: 15%" alt="">
-            <h4>Digital
-              Entrepreneurship 
-              Academy Training</h4>
+            <div class="row">
+              <div class="col-2 my-auto">
+                <img src="assets/images/xl.png" style="width: 65px; height: 65px" alt="">
+              </div>
+              <div class="col-6">
+                <span class="fw-bold" style="font-size: 25px">Digital
+                  Entrepreneurship 
+                  Academy Training</span>
+              </div>
+            </div>
             {{-- <img src="assets/images/heading-line-dec.png" alt=""> --}}
             <p>Pelatihan Bimbingan Teknis Digital Entrepreneurship Academy (Offline) merupakan rangkaian program Digital Talent Scholarship yang dilaksanakan Badan Penelitian dan Pengembangan SDM Kementerian Komunikasi dan Informatika pada tahun 2020. 
             </p>
@@ -88,22 +94,22 @@
 
   <div class="container">
     <div class="section-heading">
-      <h4>It's <em>Available</em> Now!</h4>
+      <h4 class="text-center my-5">It's <em>Available</em> Now!</h4>
     </div>
-    <div class="row">
-      <div class="col-lg-3">
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-3 mx-4">
         <div class="service-item first-service">
           <img src="assets/images/xl.png" style="width: 28%; height: 28%" alt="">
           <p>Plan My Future helps you to identify your interests, talents and skills that will be useful in the world of work</p>
         </div>
       </div>
-      <div class="col-lg-3">
+      <div class="col-lg-3 mx-4">
         <div class="service-item second-service">
           <img src="assets/images/xl.png" style="width: 28%; height: 28%" alt="">
           <p>Plan My Future provides psychological questions that have been tested in various psychological tests in Indonesia</p>
         </div>
       </div>
-      <div class="col-lg-3">
+      <div class="col-lg-3 mx-4">
         <div class="service-item third-service">
           <img src="assets/images/xl.png" style="width: 28%; height: 28%" alt="">
           <p>Plan My Future provides psychological test results in less than a week</p>
