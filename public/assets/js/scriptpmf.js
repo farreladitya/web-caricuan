3b21d5f8c848ffcfd1164b3eb5508e6fb75328ff
Firdaus Wahyu Gama Ashaari <90119969+Firdagama18@users.noreply.github.com>
2022-05-20 00:11:42 +0700

    pmf newest

--- public/assets/js/scriptpmf.js
+++ public/assets/js/scriptpmf.js
@@ -170,35 +170,35 @@ function showResult() {
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-//     if (userScore > 3) {
-//         // if user scored more than 3
-//         //creating a new span tag and passing the user score number and total question number
-//         let scoreTag =
-//             "<span>and congrats! , You got <p>" +
-//             userScore +
-//             "</p> out of <p>" +
-//             questions.length +
-//             "</p></span>";
-//         scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
-//     } else if (userScore > 1) {
-//         // if user scored more than 1
-//         let scoreTag =
-//             "<span>and nice , You got <p>" +
-//             userScore +
-//             "</p> out of <p>" +
-//             questions.length +
-//             "</p></span>";
-//         scoreText.innerHTML = scoreTag;
-//     } else {
-//         // if user scored less than 1
-//         let scoreTag =
-//             "<span> You got <p>" +
-//             userScore +
-//             "</p> out of <p>" +
-//             questions.length +
-//             "</p></span>";
-//         scoreText.innerHTML = scoreTag;
-//     }
+    if (userScore > 8) {
+        // if user scored more than 3
+        //creating a new span tag and passing the user score number and total question number
+        let scoreTag =
+            "<span> Your personallity match with IT Job <p>" 
+            // userScore +
+            // "</p> out of <p>" +
+            // questions.length +
+            // "</p></span>";
+        scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
+    } else if (userScore > 5) {
+        // if user scored more than 1
+        let scoreTag =
+            "<span> Your personallity match with Management Job <p>" 
+            // userScore +
+            // "</p> out of <p>" +
+            // questions.length +
+            // "</p></span>";
+        scoreText.innerHTML = scoreTag;
+    } else {
+        // if user scored less than 1
+        let scoreTag =
+            "<span> Your personallity match with Arts Job <p>" 
+            // userScore +
+            // "</p> out of <p>" +
+            // questions.length +
+            // "</p></span>";
+        scoreText.innerHTML = scoreTag;
+    }
 }
 
 function startTimer(time) {
