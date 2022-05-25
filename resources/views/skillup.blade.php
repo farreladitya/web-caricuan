91377db0d05b9a6f1169577388acabae03a2ecdb
raihandanaufal <raihandanaufal@gmail.com>
2022-05-25 10:47:40 +0700

    Revert "apply navbar"
    This reverts commit b2ee12ed9e964780ce1ada6ace39cb3216474d29.

--- resources/views/skillup.blade.php
+++ resources/views/skillup.blade.php
@@ -159,6 +159,7 @@
             <img src="assets/images/karier.mu.png" style="width: 80%; height: 80%; display:block; margin-left: auto; margin-right:auto" alt="">
             <p class="kotak"> <b>Topic:</b>
               <br>
+<<<<<<< Updated upstream
               Improving Your Excel Skills<br>  <b>Organizer:</b> <br> Karier.mu <br> <b>Speakers:</b> <br>Alexander Amando Tanoesoedibjo
             </p>
             <div class="d-flex justify-content-center">
@@ -167,6 +168,101 @@
           </div>
         </div>
       </div>
+=======
+              Optimization of Communication Skills <br> Organizer: <br> Karier.mu <br> Speakers: <br>Stephanus Avata, S.I.Kom.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div id="about" class="about-us section" style="margin-left: 320px">
+    {{-- <div class="container"> --}}
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading">
+            <div class="row">
+              <div class="col-2 my-auto">
+                <img src="assets/images/digital.png" style="width: 80px; height: 80px; display:block; margin-left: auto; margin-right:auto" alt="">
+              </div>
+              <div class="col-6">
+                <span class="fw-bold" style="font-size: 25px">Digital
+                  Entrepreneurship
+                  Academy Training</span>
+              </div>
+            </div>
+            {{-- <img src="assets/images/heading-line-dec.png" alt=""> --}}
+            <p>Pelatihan Bimbingan Teknis Digital Entrepreneurship Academy (Offline) merupakan rangkaian program Digital Talent Scholarship yang dilaksanakan Badan Penelitian dan Pengembangan SDM Kementerian Komunikasi dan Informatika pada tahun 2020.
+            </p>
+          </div>
+          <div class="row">
+            {{-- <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">With the Best Mentor</a></h4>
+                <p>Certified Mentors</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Video Access</a></h4>
+                <p>Everywhere & Anytime</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Create Schedule</a></h4>
+                <p>Customize your Schedule</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Latest Video</a></h4>
+                <p>Everyday</p>
+              </div>
+            </div> --}}
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-3 mx-4">
+        <div class="service-item first-service">
+          <img src="assets/images/ypia.png" style="width: 60%; height: 60%; display:block; margin-left: auto; margin-right:auto" alt="">
+          <p>Topic:
+            <br>
+            Effective Report Writing for Internal Audit <br> Organizer: <br> PPIA <br> Speakers: <br>Dr. Ir. Mudjahidin, S.T., M.T
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 mx-4">
+        <div class="service-item second-service">
+          {{-- <img src="{{URL::asset('/assets/images/xl.png')}}" width="28%" height="28%" class="float-right"> --}}
+          <img src="assets/images/web.png" style="width: 30%; height: 30%; display:block; margin-left: auto; margin-right:autos " alt="">
+          <p>Topic:
+            <br>
+            Learning and Teaching In Pandemi Era <br> Organizer: <br> Whiteboard Education<br> Speakers: <br>Eric Kunto Ariwibowo, M.Pd
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 mx-4">
+        <div class="service-item third-service">
+          <img src="assets/images/karier.mu.png" style="width: 80%; height: 80%; display:block; margin-left: auto; margin-right:auto" alt="">
+          <p>Topic:
+            <br>
+            Improving Your Excel Skills<br> Organizer: <br> Karier.mu <br> Speakers: <br>Alexander Amando Tanoesoedibjo
+          </p>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </div>
   </div>
 </div>
